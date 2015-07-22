1128eb27806bc3ee5acdd771daf03410236fa726
Mattia D'Alleva <antaresone@antaresone.eu>
2015-07-22 23:44:57 +0200

    common: always enable ADB authentication

--- config/common.mk
+++ config/common.mk
@@ -66,10 +66,8 @@ PRODUCT_PROPERTY_OVERRIDES += \
 # Thank you, please drive thru!
 PRODUCT_PROPERTY_OVERRIDES += persist.sys.dun.override=0
 
-ifneq ($(TARGET_BUILD_VARIANT),eng)
 # Enable ADB authentication
 ADDITIONAL_DEFAULT_PROPERTIES += ro.adb.secure=0
-endif
 
 # Copy over the changelog to the device
 PRODUCT_COPY_FILES += \
