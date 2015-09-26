ea70ee172dce7aef2ed446f280f56abfcd413d8e
Grzmot22 <grzmot22@gmail.com>
2015-09-26 16:50:28 +0200

    Add SuperSu(this time for real)

--- config/common.mk
+++ config/common.mk
@@ -122,7 +122,9 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     vendor/cm/prebuilt/common/UPDATE-SuperSU.zip:system/addon.d/UPDATE-SuperSU.zip \
     vendor/cm/prebuilt/common/etc/init.d/99SuperSUDaemon:system/etc/init.d/99SuperSUDaemon
-
+# SuperSU
+PRODUCT_COPY_FILES += \
+    vendor/cm/prebuilt/common/supersu/supersu.zip:system/supersu/supersu.zip
 # KernelAdiutor
 PRODUCT_COPY_FILES += \
     vendor/cm/prebuilt/KernelAdiutor/KernelAdiutor.apk:system/app/KernelAdiutor/KernelAdiutor.apk
