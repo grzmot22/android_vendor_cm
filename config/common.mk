70d01ec3e77f5e47d1dff364d7648ffa7811ef7b
Luis Vidal <lvidal@cyngn.com>
2016-03-21 10:18:29 -0700

    Add Weather Content Provider [3/5]
    Introduce the weather system feature, which will be used to
    identify if the Weather Content Provider/Weather services are
    available in the device.
    
    Add SELinux entries for the cmweather service
    
    Change-Id: Ibe862903095276f87f23c0d7dae54733eeeb5638

--- config/common.mk
+++ config/common.mk
@@ -114,7 +114,8 @@ PRODUCT_PACKAGES += \
     CyanogenSetupWizard \
     CMSettingsProvider \
     ExactCalculator \
-    LiveLockScreenService
+    LiveLockScreenService \
+    WeatherProvider
 
 # Exchange support
 PRODUCT_PACKAGES += \
